dc3901ab0808c4077fb721454756e93bcd8c0d59
978-9-17-637879-3 <102838865+978-9-17-637879-3@users.noreply.github.com>
2023-08-04 17:51:14 -0400

    Format & clean apply.js, update move format

--- apply.js
+++ apply.js
@@ -1,32 +1,29 @@
 const robot = require('robotjs');
 const fs = require('fs');
 
-const startX = 102-64;
-const gap = 64;
+const startX = 171 - 64;
+const gap = 62;
 const bottleCount = 15;
 
 const bottleToPx = {}
 for (let i = 0; i < bottleCount; i++) {
-    bottleToPx[i] = {x: startX + i*gap, y: 335};
+    bottleToPx[i] = { x: startX + i * gap, y: 353 };
 }
 console.log(bottleToPx)
 
 const wait = (timeMS) => new Promise(resolve => setTimeout(resolve, timeMS));
-
-robot.setMouseDelay(2);
+const moveParser = (moveString) => { const noParentheses = moveString.replaceAll(/(\(|\))/g, "").split(" "); return [Number(noParentheses[0]), Number(noParentheses[1])] }
 
 let sol = fs
     .readFileSync('./sol.txt')
     .toString()
     .trim()
-    .split('\n')
-    .map(s => s.trim());
+    .split('->')
 console.log(sol);
 (async function () {
     for (const move of sol) {
         console.log(move)
-        const from = Number(move.split(' ')[0]);
-        const to = Number(move.split(' ')[1]);
+        const [from, to] = moveParser(move);
         const pxFrom = bottleToPx[from];
         const pxTo = bottleToPx[to];
 
